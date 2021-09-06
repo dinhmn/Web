d71f398eaf7c878ff10977297aafc8a674a3bd42
Mai Ngọc Định <85937956+ngocdinh2k1@users.noreply.github.com>
2021-09-06 17:36:29 +0700

    06/09/2021

---
+++ MySite/1.js
@@ -0,0 +1,21 @@
+﻿ // truy xuat cai nut
+
+ // truoc khi truy xuat phai document ready
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    var nut = document.getElementById('nut1');
+    var khoi = document.getElementsByClassName('card');
+    console.log(khoi[0])
+
+    // go su kien click
+
+    nut.onclick = function() {
+        khoi[0].classList.toggle('diphai');
+    }
+
+}, false)
+
+
+
